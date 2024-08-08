c2ac37f3451d7f7b3a1606df6c41c6c26f5a935c
TestingMaestro <forsocialmediausageonly@gmail.com>
2024-08-08 15:56:12 +0530

    Lab002 is added

---
+++ src/ex_08082024/Lab001.py
@@ -0,0 +1,7 @@
+# Please provide code info below --->comment
+
+print("Hello!! This is my first python program")
+print("Division:",2/2)
+print(3+4)
+print("Duplicate Addition:",3+4)
+print("Subtraction:",90-20)
---
+++ src/ex_08082024/Lab002.py
@@ -0,0 +1,1 @@
+print("Hello Yashodhar, Welcome to python batch")
