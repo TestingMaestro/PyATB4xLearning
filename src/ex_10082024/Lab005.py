f8c1ed06dea1819a16bf9ca7154c58e48b4916c2
Yashodhar Karki <yashodhar.karkii@gmail.com>
2024-08-12 20:25:40 +0530

    Lab005 file is added

---
+++ src/ex_10082024/Lab005.py
@@ -0,0 +1,10 @@
+age=25
+
+print("Age = ",age)
+
+# Determines type of a variable at runtime [Dynamically Typed]
+print("Age type",type(age))
+
+age = "twenty - five"
+
+print("Age Type", type(age))
