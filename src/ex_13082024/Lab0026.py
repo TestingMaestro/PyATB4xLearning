a1628d1fc485473b9e6db22d66c7b7aa66374d78
Yashodhar Karki <yashodhar.karkii@gmail.com>
2024-08-16 10:59:08 +0530

    Files added

---
+++ src/ex_13082024/Lab0026.py
@@ -0,0 +1,24 @@
+# Strings
+
+name = "Yashodhar"
+
+print(name.upper())
+print(name.lower())
+print(len(name))
+
+age = "90"
+print(type(age))
+
+name = name+str(1) # Concatenation
+print(name)
+
+firstName = "Yashodhar"
+lastName = "karki"
+
+res = firstName+lastName
+print(res)
+
+
+how_many_planes_i_have = None
+print(how_many_planes_i_have)
+print(type(how_many_planes_i_have))
